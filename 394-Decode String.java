f97c7e42127c61d91c0c894a314715839214e360
Mindy927 <lionetmin@icloud.com>
2018-06-26 22:55:43 +0800

    stack

---
+++ 394-Decode String.java
@@ -0,0 +1,50 @@
+/*Given an encoded string, return it's decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Examples:
+
+s = "3[a]2[bc]", return "aaabcbc".
+s = "3[a2[c]]", return "accaccacc".
+s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+
+Author: Mindy927 */
+
+
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        
+        int num = 0;
+        String result = "";
+        for (int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) num = num*10 + c -'0';
+            else if (c == '['){ //push to stack when '['   
+                numStack.push(num);
+                num = 0;
+                strStack.push(result);
+                result = "";
+            }else if (c == ']'){ // result = strStack.pop() + count * result
+                int count = numStack.pop();
+                StringBuilder sb = new StringBuilder();
+                sb.append(strStack.pop());
+                while (count > 0) {
+                    sb.append(result);
+                    count--;
+                }
+                result = sb.toString();
+            }else {
+                result = result + s.charAt(i);
+            }
+        }
+        
+        return result;
+    }
+}
