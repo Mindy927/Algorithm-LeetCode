ed0acdda573edf5986d8e3de916b42325338b097
Mindy927 <lionetmin@icloud.com>
2018-08-08 23:11:27 +0800

    dfs

---
+++ 78-Subsets.java
@@ -0,0 +1,43 @@
+/*
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+Author: Mindy927 */
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        dfs(nums, 0, temp, result);
+        return result;
+    }
+    
+    public void dfs(int[] nums, int start, List<Integer> temp, List<List<Integer>> result){
+        result.add(new ArrayList<>(temp));
+        if (start == nums.length) return;
+        
+        for (int i=start; i<nums.length; i++){ //num to add in this round can be any number from start
+            //handle duplicate, consider case [1 2 2], add first 2 and second 2 in this round is same
+            if (i>0 && nums[i] == nums[i-1] && i!=start) continue; 
+            temp.add(nums[i]);
+            dfs(nums, i+1, temp, result);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
