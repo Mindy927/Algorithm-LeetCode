50a5b3ce16fd7e8837128d2e6610d1e947f6a797
Mindy927 <lionetmin@icloud.com>
2018-08-10 11:33:16 +0800

    recursion, iteration

---
+++ 94-Binary Tree Inorder Traversal.java
@@ -0,0 +1,59 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+Author: Mindy927*/
+
+//Method 1 : recursion
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        
+        List<Integer> left = inorderTraversal(root.left);
+        List<Integer> right = inorderTraversal(root.right);
+        
+        for (int l:left) res.add(l);
+        res.add(root.val);
+        for (int r:right) res.add(r);
+        
+        return res;
+    }
+}
+
+//Method 2: Iteration
+/*
+-- push all the cur.left to stack
+-- pop(), add to res
+-- cur = cur.right
+*/
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        
+        TreeNode cur = root;
+        while (cur!=null || !stack.isEmpty()){
+            while (cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        
+        return res;   
+    }
+}
