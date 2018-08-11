0b2fc5f3a101104e495be62833dfa3e65a37d4f1
Mindy927 <lionetmin@icloud.com>
2018-08-12 00:00:32 +0800

    heap, binary search

--- 378-Kth Smallest Element in a Sorted Matrix.java
+++ 378-Kth Smallest Element in a Sorted Matrix.java
@@ -17,7 +17,7 @@ return 13.
 Author: Mindy927*/
 
 
-//Merge sort
+//Method 1: heap
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int m = matrix.length, n = matrix[0].length;
@@ -43,3 +43,36 @@ class Solution {
         return matrix[kth[0]][kth[1]];
     }
 }
+
+
+//Method 2: binary search all possible result (min to max, k = max - min), and verify O(nlgk)
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = matrix[0][0], right = matrix[m-1][n-1];
+        
+        while (left +1 < right){
+            int mid = left + (right - left)/2;
+            int cnt = count(matrix, mid);
+            //if (cnt == k) return mid; we cannot return mid here since they might be number X <mid which has count(X) ==k, need to search further!
+            if (cnt < k) left = mid; 
+            else right = mid;
+        }
+        
+        return count(matrix,left)>=k? left:right;
+    }
+
+    public int count(int[][] matrix, int target){//# of elements <= target
+        int cnt = 0;
+        for (int i=0; i<matrix.length; i++){
+            if (matrix[i][0] > target) break;
+            for (int j=0; j<matrix[0].length; j++){
+                if (matrix[i][j] > target) break;
+                else cnt++;
+            }
+        }
+       return cnt;
+    }
+}
+
