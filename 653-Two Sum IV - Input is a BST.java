540b3eba71b38aadc4a43d64d1386ff8c7db9b5c
Mindy927 <lionetmin@icloud.com>
2018-08-14 18:30:49 +0800

    hashset, two pointers

--- 653-Two Sum IV - Input is a BST.java
+++ 653-Two Sum IV - Input is a BST.java
@@ -36,6 +36,8 @@ Aurhor: Mindy927*/
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+//hashset O(n), O(n)
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
         if (root == null) return false;
@@ -53,5 +55,29 @@ class Solution {
             }
         }
         return false;    
+    }
+}
+
+//convert to in-order list and use two pointers
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        int left=0, right=list.size()-1;
+        while (left < right){
+            int sum = list.get(left) + list.get(right);
+            if (sum == k) return true;
+            if (sum < k) left ++;
+            else right--;
+        }
+        
+        return false;
+    }
+    
+    public void helper(TreeNode root, List<Integer> list){
+        if (root == null) return;
+        helper(root.left, list);
+        list.add(root.val);
+        helper(root.right, list);
     }
 }
