72fdf0de4c8f795adb7131bf744f86a961f97760
Mindy927 <lionetmin@icloud.com>
2018-08-14 17:20:55 +0800

    bit manipulation

--- 461-Hamming Distance.java
+++ 461-Hamming Distance.java
@@ -29,3 +29,15 @@ class Solution {
         return Integer.bitCount(x^y);
     }
 }
+
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int cnt = 0;
+        for (int i=0; i<32; i++){
+            cnt += (x & (1<<i)) != (y & (1<<i))? 1:0;
+        }
+        
+        return cnt;
+    }
+}
