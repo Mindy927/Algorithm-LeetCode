270a5f7a3447db14e99057b0e7e5b0854a9e9ca1
Mindy927 <lionetmin@icloud.com>
2018-08-15 19:15:39 +0800

    bucket sort

--- 347-Top K Frequent Elements.java
+++ 347-Top K Frequent Elements.java
@@ -10,7 +10,7 @@ Your algorithm's time complexity must be better than O(n log n), where n is the 
 
 Author: Mindy927 */
 
-
+//Method 1: hashmap + heap: time: O(nlgn)
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>(); // value:cnt
@@ -41,3 +41,37 @@ class Solution {
         return result;
     }
 }
+
+
+//method 2: bucket sort, O(n)
+//bucket[i] : a list of nums with frequency i
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        Map<Integer,Integer>  map = new HashMap<>(); //val: cnt
+        
+        for (int num:nums){
+            map.put(num, map.containsKey(num)? map.get(num)+1:1);
+        }
+        
+        List<Integer>[] bucket = new List[nums.length+1];
+        
+        for (int key:map.keySet()){
+            int cnt = map.get(key);
+            if (bucket[cnt] == null) bucket[cnt] = new ArrayList<>();
+            bucket[cnt].add(key);
+        }
+        
+        int cnt = 0;
+        for (int i=nums.length; i>=0; i--){
+            List<Integer> temp = bucket[i];
+            if (temp == null) continue;
+            for (int val:temp){
+                if (cnt++ < k) result.add(val);
+                else break;
+            }
+            if (cnt >= k) break;
+        }
+        
+        return result;
+
