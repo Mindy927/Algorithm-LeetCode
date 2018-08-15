610c751d80a240da0c07813fb0f577bba9152277
Mindy927 <lionetmin@icloud.com>
2018-08-15 18:33:15 +0800

    dp

--- 377-Combination Sum IV.java
+++ 377-Combination Sum IV.java
@@ -25,20 +25,25 @@ How does it change the problem?
 What limitation we need to add to the question to allow negative numbers?
 
 */
+
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target+1];
         
-        for(int num:nums) if (num<target+1) dp[num]=1; //corner case when initialize (dp[num]=1) : num < target+1
-        
-        for (int i=1; i<=target; i++){
+        dp[0] = 1;
+        for (int i=0; i<target+1; i++){
             for (int num:nums){
-                if (i-num < 0) continue;
-                dp[i] += dp[i-num];
+                if (i - num >=0) dp[i] += dp[i-num];
             }
         }
         
         return dp[target];
     }
-    
 }
+
+
+/*follow up
+for negative numbers: combinations could be potentially of infinite length. e.g, nums = [-1, 1] and target = 1
+solution: limit the length of combination sequence
+use 2-D dp, dp[target, length]
+*/
