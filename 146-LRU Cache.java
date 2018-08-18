6017bd986e09e48f9467939d2d4c1ab323d4e255
Mindy927 <lionetmin@icloud.com>
2018-08-18 21:31:57 +0800

    design

--- 146-LRU Cache.java
+++ 146-LRU Cache.java
@@ -1,5 +1,4 @@
 /*
-
 Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
 
 get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
@@ -10,7 +9,7 @@ Could you do both operations in O(1) time complexity?
 
 Example:
 
-LRUCache cache = new LRUCache( 2 /* capacity */ );
+LRUCache cache = new LRUCache( 2  capacity  );
 
 cache.put(1, 1);
 cache.put(2, 2);
@@ -22,82 +21,78 @@ cache.get(1);       // returns -1 (not found)
 cache.get(3);       // returns 3
 cache.get(4);       // returns 4
 
-Author:Mindy927 */
-
-public class LRUCache {
-    /*
-    Double Linked List to keep tack of requests, most recent -> least recent
-    HashMap to store key:value 
-    Move nodes to beginning of list when requested (delete node & add node again)
+Author:Mindy927  */
+ /*
+    Double Linked List to keep tack of keys requested, least recent -> most recent
+    HashMap to store key:DNode 
+    Move nodes to end of list when requested (delete node & add node again)
     */
-    class Dnode{
-        Dnode prev;
-        Dnode next;
-        int key;
+    
+class LRUCache {
+    class DNode{ //doubly linked list node
         int val;
-        public Dnode(int key, int val){
+        int key;
+        DNode prev = null;
+        DNode next = null;
+        public DNode(){}
+        public DNode(int key, int val){
             this.key = key;
             this.val = val;
         }
     }
-            //Always inseart after head
-    public void add(Dnode node){
-            node.next = head.next;
-            node.prev = head;
-            head.next.prev = node;
-            head.next = node;
-        }
-   
-    public void delete(Dnode node){
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-    }   
-        //Move node in between -> after head
-    public void move(Dnode node){
-            delete(node);
-            add(node);
-        }
-        
-    int capacity;
-    Map<Integer,Dnode> map;
-    Dnode head, tail;
-    int count;
-    public LRUCache(int capacity) { //Remember to link head to tail when initialize
-        this.capacity = capacity;
+    
+    //add before tail, FIFO
+    public void add(DNode node){
+        tail.prev.next = node;
+        node.prev = tail.prev;
+        node.next = tail;
+        tail.prev = node;
+    }
+    
+    //delete node
+    public void delete(DNode node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    //move node to tail
+    public void move(DNode node){
+        delete(node);
+        add(node);
+    }
+    
+    Map<Integer, DNode> map;
+    DNode head, tail;
+    int cnt,capacity;
+    public LRUCache(int capacity) {
         map = new HashMap<>();
-        head = new Dnode(0,0);
-        head.prev = null;
-        tail = new Dnode(0,0);
-        tail.next = null;
+        head = new DNode(0,0);
+        tail = new DNode(0,0);
         head.next = tail;
         tail.prev = head;
-        count = 0;
+        cnt = 0;
+        this.capacity = capacity;
     }
     
     public int get(int key) {
-        if (map.containsKey(key)){
-            //update req list
-            Dnode node = map.get(key);
-            move(node);
-            return node.val;
-        } else return -1;
+        if (!map.containsKey(key)) return -1;
+        move(map.get(key)); //move to tail
+        return map.get(key).val;
     }
     
     public void put(int key, int value) {
-        if (get(key)!= -1){//update existing key
-            map.get(key).val = value;
-            return;
+        if (get(key) != -1){ //update existing node
+            map.get(key).val = value; 
         }else{
-            Dnode node = new Dnode(key, value);
-            map.put(key, node);
+            DNode node = new DNode(key,value);
             add(node);
-            count++;
-            if (count > capacity) {
-                map.remove(tail.prev.key);
-                delete(tail.prev);
-                count--;
+            map.put(key, node);
+            cnt++;
+            if (cnt > capacity) { //remove head.next 
+                map.remove(head.next.key); 
+                delete(head.next);
+                cnt--;
             }
-            return;
         }
     }
 }
