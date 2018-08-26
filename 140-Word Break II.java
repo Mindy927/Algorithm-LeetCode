2ce3efa1950c28b3fc445ae320625f3e2e4711df
Mindy927 <lionetmin@icloud.com>
2018-08-26 23:11:35 +0800

    dfs with memorization

--- 140-Word Break II.java
+++ 140-Word Break II.java
@@ -1,35 +1,74 @@
 /*
-Method 1: DFS
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
 
+Note:
 
-/*
-Method 2: DP
-dp[i]: a list of string that formed by s.substring[0,i]
-*/
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input:
+s = "catsanddog"
+wordDict = ["cat", "cats", "and", "sand", "dog"]
+Output:
+[
+  "cats and dog",
+  "cat sand dog"
+]
+Example 2:
+
+Input:
+s = "pineapplepenapple"
+wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+Output:
+[
+  "pine apple pen apple",
+  "pineapple pen apple",
+  "pine applepen apple"
+]
+Explanation: Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input:
+s = "catsandog"
+wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output:
+[]
+
+Author: Mindy927*/
+
 
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
+        Map<String, List<String>> map = new HashMap<>(); //string:all possible word break
         Set<String> set = new HashSet<>();
-        for(String word:wordDict) set.add(word);
-        List<String>[] dp = new LinkedList[n];
+        for (String word:wordDict) set.add(word);
         
-        for(int i=0; i<s.length(); i++){
-            List<String> list = new LinkedList<>();
-            for (int j=0; j<=i; j++){
-                String temp = s.substring(j, i+1);
-                if (set.contains(temp)){
-                    if (j==0) list.add(temp); //from start of string
-                    else {
-                        for (String str: dp[j-1]){ //combine str[j,i] with str[0,j-1]
-                            list.add(str+" "+temp);
-                        }
-                    }
-                }
-            }
-            dp[i] = list;
+        return dfs(s, set, map);
+    }
+    
+    public List<String> dfs(String s, Set<String> set, Map<String, List<String>> map){
+        if (map.containsKey(s)) return map.get(s);
+        
+        List<String> result = new ArrayList<>();
+        if (s.length() == 0){//reach end of string, return "" for previous strings to append themselves
+            result.add("");
+            return result;
         }
         
-        return dp[n-1];
+        for (int i=0; i<s.length(); i++){ 
+            String word = s.substring(0,i+1);
+            if (set.contains(word)){ //dict contains word s[i,j]
+                List<String> strs = dfs(s.substring(i+1), set, map);
+                for (String str:strs){
+                    if (str.equals("")) result.add(word); //last word
+                    else result.add(word + " " + str);
+                }
+            }
+            
+        }
+        map.put(s, result);
+        return result;
     }
+    
 }
