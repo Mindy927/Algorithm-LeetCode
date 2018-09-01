94ede93cc2f0d9a9b9a2f707f139a88f4d38492b
Mindy927 <lionetmin@icloud.com>
2018-09-01 13:24:41 -0400

    linked list

--- 24-Swap Nodes in Pairs.java
+++ 24-Swap Nodes in Pairs.java
@@ -38,3 +38,28 @@ class Solution {
         return next;
     }
 }
+
+
+//Method 2: prev -> node1 -> node2, swap node1 & node2, move prev to node1 for next round
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode node1 = prev.next;
+        
+        while (node1 !=null && node1.next != null){
+            ListNode node2 = node1.next;
+            ListNode next = node2.next;
+            prev.next = node2;
+            node2.next = node1;
+            node1.next = next;
+            prev = node1;
+            node1 = next;
+        }
+        
+        return dummy.next;
+    }
+}
