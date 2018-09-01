14b1877f0d83e9d60db5a9d9a4d457d0f50a5e34
Mindy927 <lionetmin@icloud.com>
2018-08-31 20:50:50 -0400

    array

--- 4-Median of Two Sorted Arrays.java
+++ 4-Median of Two Sorted Arrays.java
@@ -34,17 +34,22 @@ class Solution {
     }
     
     public double findKth(int[] nums1, int start1, int[] nums2, int start2, int k){
+        //if num1 is exhausted, return kth number in nums2
         if (start1 >= nums1.length) return nums2[start2 + k - 1];
         if (start2 >= nums2.length) return nums1[start1 + k - 1];
+        // If k == 1, return the first number
+        // Since nums1 and nums2 is sorted, the smaller one among the start point of nums1 and nums2 is the first one
         if (k == 1) return Math.min(nums1[start1], nums2[start2]);
         
         int mid1 = Integer.MAX_VALUE, mid2 = Integer.MAX_VALUE;
-        if (start1 + k/2 - 1 < nums1.length) mid1 = nums1[start1 + k/2 - 1]; 
+        if (start1 + k/2 - 1 < nums1.length) mid1 = nums1[start1 + k/2 - 1];  //k/2 elements from start1(including)
         if (start2 + k/2 - 1 < nums2.length) mid2 = nums2[start2 + k/2 - 1];
-        if (mid1 < mid2){   //remove k/2 numbers at each iteration, start from start1 + k/2 not start1 + k/2 - 1 
-            return findKth(nums1, start1 + k/2, nums2,start2,           k - k/2); 
+
+        //remove k/2 numbers at each iteration, start from start1 + k/2 not start1 + k/2 - 1 
+        if (mid1 < mid2){   
+            return findKth(nums1, start1 + k/2, nums2,start2,           k - k/2);  //nums1.right + nums2
         }else{
-            return findKth(nums1, start1,           nums2, start2+ k/2 , k - k/2);
+            return findKth(nums1, start1,           nums2, start2+ k/2 , k - k/2); //nums2.right + nums1
         }
             
     }
