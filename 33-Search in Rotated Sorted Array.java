28f01ee45eb134dac003b1e445f735e98545f3eb
Mindy927 <lionetmin@icloud.com>
2018-09-02 15:15:17 -0400

    sort

--- 33-Search in Rotated Sorted Array.java
+++ 33-Search in Rotated Sorted Array.java
@@ -20,23 +20,27 @@ Output: -1
 
 Author: Mindy927*/
 
-//consider case when mid falls at first half or second half
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) return -1;
         int left = 0, right = nums.length-1;
+        
+        if (nums.length == 0) return -1;
         while (left+1 < right){
             int mid = left + (right - left)/2;
             if (nums[mid] == target) return mid;
-            if (nums[mid] >= nums[left]){ 
-                if (nums[mid] < target || (nums[right] < nums[left] && target <= nums[right])) left = mid; //corner case when array is not rotated
-                else right = mid;
-            } else { //nums[mid] < nums[left]
-                if (nums[mid] > target || (nums[right] < nums[left] && target >= nums[left])) right = mid;
+            
+            //left part is sorted, when target falls in [left, mid], move right to mid
+            if (nums[mid] > nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) right = mid;
                 else left = mid;
+            }
+            //right part is sorted, when target falls in [mid, right], move left to mid
+            else {
+                if (target > nums[mid] && target <= nums[right]) left = mid;
+                else right = mid;
             }
         }
         
-        return nums[left]==target? left:nums[right]==target? right:-1;
+        return nums[left] == target? left:nums[right]==target? right:-1;
     }
 }
