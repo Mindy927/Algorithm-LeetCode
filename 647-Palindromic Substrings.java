4d4be29a8273f68ea39ef1a6a6cd55533a73b461
Mindy927 <lionetmin@icloud.com>
2018-09-03 08:55:25 -0400

    string; dp

--- 647-Palindromic Substrings.java
+++ 647-Palindromic Substrings.java
@@ -16,6 +16,7 @@ The input string length won't exceed 1000.
 
 Author: Mindy927 */
 
+//Method 1: expand at all possible centers
 class Solution {
     public int countSubstrings(String s) {
         int cnt = 0;
@@ -34,6 +35,33 @@ class Solution {
             cnt++;
             i--; j++;
         }
+        return cnt;
+    }
+}
+
+//Method 2: dp
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n]; //dp[i][j]: is s.substring(i,j+1) a palindromic
+        
+        /*
+        if charAt(i) == charAt(j), dp[i][j] = dp[i+1][j-1] 
+        */
+        
+        int cnt = 0;
+        for (int i=n-1; i>=0; i--){
+            for (int j=i; j<n; j++){
+                if (i==j) dp[i][j] = true; //a single char
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (i+1 == j) dp[i][j] = true; //i,j adjacent
+                    else if (i+1 <= j-1) dp[i][j] = dp[i+1][j-1]; //i+1==j-1, center is a char, e.g, aba
+                }
+                if (dp[i][j]) cnt++;
+            }
+            
+        }
+        
         return cnt;
     }
 }
