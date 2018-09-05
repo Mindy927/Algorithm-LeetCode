a7fd41a5fdeb4af3fb8b7c49631cc266938b7b7a
Mindy927 <lionetmin@icloud.com>
2018-09-05 15:08:51 -0400

    sliding window

--- 3-Longest Substring Without Repeating Characters.java
+++ 3-Longest Substring Without Repeating Characters.java
@@ -10,18 +10,21 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 Author: Mindy927*/
-
 class Solution {
+    //move left pointer to last index of current char when necessay
+    //e.g, a b c d b (for second b, move left pointer to first b)
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>(); //char:latest index
-        int prev = -1, max =0;
+        Map<Character, Integer> map = new HashMap<>(); //char:previous index of this char
+        
+        int res = 0;
+        int prev = -1; //last index curChar OR previouly left pointer
         for (int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            prev = map.containsKey(c)? Math.max(map.get(c),prev):prev; //if map.containsKey(c), prev is max(prev, map.get(c))
-            max = Math.max(max, i-prev);
-            map.put(c,i);
+            if (map.containsKey(c)) prev = Math.max(prev, map.get(c));
+            res = Math.max(i-prev, res);
+            map.put(c, i);
         }
         
-        return max;
+        return res;
     }
 }
