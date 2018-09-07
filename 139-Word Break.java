dd72d60f5e2a6522f9b6f8736364d0c6e5b339fa
Mindy927 <lionetmin@icloud.com>
2018-09-06 20:43:53 -0400

    dp; recursion

--- 139-Word Break.java
+++ 139-Word Break.java
@@ -20,24 +20,52 @@ Explanation: Return true because "applepenapple" can be segmented as "apple pen 
 Author: Mindy927
 */
 
+//Method 1:dp, time O(n^2), space O(n)
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>();
-        for (String word:wordDict){
-            set.add(word);
-        }
         
-        int n = s.length();
-        boolean[] dp = new boolean[n+1];
-        //dp[i] is true if s.substring(i,j) are in dict && dp[j] is true
+        for(String word:wordDict) set.add(word);
+        
+        boolean[] dp = new boolean[s.length()+1]; //dp[i] is true if s.substring(0,i) has valid word break
         dp[0] = true;
-        for (int i=1; i<n+1; i++){
-            for (int j=i; j<n+1; j++){
-                String word = s.substring(i-1,j);
-                if (set.contains(word) && dp[i-1]) dp[j] = true;
+        
+        for(int i=1; i<s.length()+1; i++){
+            for (int j=0; j<i; j++) {
+                String word = s.substring(j,i);
+                if (set.contains(word)) {
+                    //dp[i] is true if s.substring(i,j) are in dict && dp[j] is true
+                    dp[i] = dp[i] || dp[j]; // from [0,j-1] is valid(dp[j]) and [j,i] is valid
+                }
+            }
+        }       
+        
+        return dp[s.length()];
+    }
+}
+
+//Method 2: recursion
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] memo = new boolean[s.length()]; //memo[i]: whether s.substring(i) has valid word break
+        boolean[] visited = new boolean[s.length()]; //visited[i]:whether result for s.substring has been recorded in memo
+        return dfs(s, 0, new HashSet<>(wordDict),visited, memo);
+    }
+    
+    //whether s.substring(start) has valid word break
+    public boolean dfs(String s, int start, Set<String> set, boolean[] visited, boolean[] memo){
+        if (start == s.length()) return true;
+        if (visited[start]) return memo[start];
+        
+        visited[start] = true;
+        for (int end = start+1; end<=s.length(); end++){
+            String word = s.substring(start, end);
+            if (set.contains(word) && dfs(s, end, set, visited, memo)){
+                memo[start] = true;
+                return true;
             }
         }
-        
-        return dp[n];
+        memo[start] = false;
+        return false; 
     }
 }
