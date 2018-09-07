18256efd49fd797dd3bb0c0b3bfea34b9568519f
Mindy927 <lionetmin@icloud.com>
2018-09-07 10:23:39 -0400

    bfs

--- 490-The Maze.java
+++ 490-The Maze.java
@@ -40,46 +40,76 @@ Explanation: There is no way for the ball to stop at the destination.
 
 Author:Mindy927 */
 
-//DFS + helper func roll
-
+//Method 1: bfs, bfs is better for this case since it will stop once we found shortest path
 class Solution {
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        boolean[][] visited = new boolean[maze.length][maze[0].length];  //visited starting pointe
-        return dfs(maze, visited, start, destination);
-    }
-    
-    int[][] dirs = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
-    public boolean dfs(int[][] maze, boolean[][] visited, int[] start, int[] dest){
-         if (visited[start[0]][start[1]]) return false;
-         if (Arrays.equals(start, dest)) return true;
-         
-         visited[start[0]][start[1]] = true;
-         for (int[] dir:dirs){
-             int[] nextPos = roll(maze, start, dir);
-             if (Arrays.equals(nextPos,start)) continue;
-             if (dfs(maze,visited, nextPos, dest)) return true;
-         }
+        if (Arrays.equals(start, destination)) return true;
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        int[] DIRS = new int[]{1, 0, -1, 0, 1};
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(start);
+        visited[start[0]][start[1]] = true;
+        
+        while (!q.isEmpty()){
+            int size = q.size();
+            for (int s=0; s<size; s++){
+                int[] pos = q.poll();
+                for (int d=0; d<4; d++){
+                    int x = pos[0], y = pos[1];
+                    int deltaX = DIRS[d];
+                    int deltaY = DIRS[d+1];
+                    //find last valid position before hitting wall/obstacle, compare x + deltaX with boundary
+                    //move when next posistion is valid
+                    while( x+deltaX>=0 && y+deltaY>=0 && x+deltaX<maze.length && y+ deltaY < maze[0].length && maze[x+deltaX][y+deltaY] == 0){
+                        x += deltaX; 
+                        y += deltaY;
+                    }
+                    
+                    int[] next = new int[]{x,y};
+                    if ( x<0 || y<0 || x>=maze.length || y >= maze[0].length ) continue;
+                    if (visited[x][y]|| maze[x][y] != 0) continue;
+                    if (Arrays.equals(next, destination)) return true;
+                    
+                    visited[x][y] = true;
+                    q.offer(next);
+                }    
+            }
+        }
         
         return false;
     }
+}
+
+
+//Method 2: dfs
+class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        Set<String> visited = new HashSet<>();
+        return dfs(maze, start[0], start[1], destination[0], destination[1], visited);
+    }
     
-    public int[] roll(int[][] maze, int[] start, int[] dir){
-        int deltaX = dir[0];
-        int deltaY = dir[1];
-        int x = start[0];
-        int y = start[1]; 
-        //roll up/down
-        if (deltaY == 0){
-             while (x+deltaX >=0 && x+deltaX < maze.length && maze[x+deltaX][y] != 1 ){ // Verify whether x+deltaX is valid before updateing x
-                  x += deltaX;
-             }
-        }
-        //roll left/right
-        if (deltaX == 0){
-            while (y+deltaY>=0 && y+deltaY<maze[0].length && maze[x][y+deltaY] != 1) {
+    static final int[] DIRS = new int[]{1, 0, -1, 0, 1};
+    public boolean dfs(int[][] maze, int i, int j, int destX, int destY, Set<String> visited){
+        if (visited.contains( i + "-" + j) || i<0 || j<0 || i>=maze.length || j >= maze[0].length || maze[i][j] != 0) return false;
+        
+        if (i==destX && j == destY) return true;
+        
+        visited.add(i + "-" + j);
+        for (int d=0; d<4; d++){
+            int x = i, y = j;
+            int deltaX = DIRS[d];
+            int deltaY = DIRS[d+1];
+            //find last valid position before hitting wall/obstacle, compare x + deltaX with boundary
+            //move when next posistion is valid
+            while( x+deltaX>=0 && y+deltaY>=0 && x+deltaX<maze.length && y+ deltaY < maze[0].length && maze[x+deltaX][y+deltaY] == 0){
+                x += deltaX; 
                 y += deltaY;
-            } 
-        }           
-        return new int[]{x,y};
+            }
+            if (dfs(maze, x, y, destX, destY, visited)) return true;
+        }
+        //visited.remove(i + "-" + j);
+        
+        return false;
     }
 }
