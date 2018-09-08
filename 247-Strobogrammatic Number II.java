ee3586094ca464a0799cd15b951598d660f6b9c5
Mindy927 <lionetmin@icloud.com>
2018-09-07 21:13:25 -0400

    recursion; bfs

--- 247-Strobogrammatic Number II.java
+++ 247-Strobogrammatic Number II.java
@@ -35,3 +35,32 @@ class Solution {
         return result;
     }
 }
+
+//bfs
+class Solution {
+    public List<String> findStrobogrammatic(int n) {
+        List<String> res = new ArrayList<>();
+        if ( n == 0 ) return res;
+        if ( n%2 == 0) res.add("");
+        if ( n%2 == 1 ) {
+            res.add("0");
+            res.add("1");
+            res.add("8");
+        }
+        
+        for (int i=0; i<n/2; i++){
+            int size = res.size();
+            for (int s=0; s<size; s++){
+                String prev = res.get(0);
+                res.remove(0);
+                if (i!= n/2-1) res.add("0" + prev + "0"); //dont add  0 + str + 0 for last round, since number wont start from 0
+                res.add( "1" + prev + "1");
+                res.add( "6" + prev + "9");
+                res.add( "9" + prev + "6");
+                res.add( "8" + prev + "8");
+            }
+        }
+        
+        return res;
+    }
+}
