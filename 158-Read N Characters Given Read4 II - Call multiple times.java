d6e898df4ad9be3db5e08ea208de670d40d2a194
Mindy927 <lionetmin@icloud.com>
2018-09-15 10:02:46 -0400

    read4

--- 158-Read N Characters Given Read4 II - Call multiple times.java
+++ 158-Read N Characters Given Read4 II - Call multiple times.java
@@ -37,18 +37,22 @@ public class Solution extends Reader4 {
      * @param n   Maximum number of characters to read
      * @return    The number of characters read
      */
-    int p4 = 0; //index of cur char to be read from buf4
-    int end4 = 0; //number of avaliable values in buf4
     char[] buf4 = new char[4];
-    public int read(char[] buf, int n) { 
+    int p4 = 0; //index of chars to be read from buf4
+    int cnt4 = 0; //number of chars avaliable in buf4
+    public int read(char[] buf, int n) {
         int cnt = 0;
-        while ( cnt < n){
-            if (p4 < end4) {
-                buf[cnt++] = buf4[p4++];
-            }else{
-                end4 = read4(buf4);
-                if (end4 == 0) break;
+        while ( cnt < n ){
+            //read4 when we dont have chars to read from buf4
+            if (p4 == cnt4){
+                cnt4 = read4(buf4);
                 p4 = 0;
+            }
+            if (cnt4 == 0) break;
+            
+            //we stop copying buf4 to buf when (1) we read n chars (2)reach end of buf4:p4 = cnt4
+            while ( p4 < cnt4 && cnt < n){
+                buf[cnt++] = buf4[p4++];
             }
         }
         
