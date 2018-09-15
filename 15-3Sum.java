0c26da82e62b08c978b0b0e1613d96a42f8652a6
Mindy927 <lionetmin@icloud.com>
2018-09-14 20:39:48 -0400

    two pointers

--- 15-3Sum.java
+++ 15-3Sum.java
@@ -21,25 +21,26 @@ Author: Mindy927*/
 //iterate first number, two pointers for the rest two numbers, be aware of duplicates  O(n^2)
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 3) return res;
+        
+        int n = nums.length;
         Arrays.sort(nums);
-        for (int i=0; i<nums.length-2; i++){
-            if (i>0 && nums[i] == nums[i-1]) continue;
-            int j = i+1, k = nums.length-1;
-            while (j<k){
-                int curSum = nums[i] + nums[j] + nums[k];
-                if ( curSum == 0) {
-                    result.add(Arrays.asList(nums[i],nums[j], nums[k]));
-                    while (j<k && nums[j+1]==nums[j]) j++;
-                    while (j<k && nums[k-1]==nums[k]) k--;
+        for (int i=0; i<n; i++){
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i+1, k = n-1;
+            while ( j < k ){
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0){
+                    res.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
                     j++; k--;
-                }
-                else if (curSum < 0 ) j++;
-                else k--;
+                    while (j<k && nums[j] == nums[j-1]) j++;
+                    while (j<k && nums[k] == nums[k+1]) k--;
+                }else if (sum > 0) k--;
+                else j++;
             }
         }
         
-        return result;
+        return res;
     }
 }
-
