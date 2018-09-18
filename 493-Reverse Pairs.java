d038fb52a94036a9177ec9d3248c9481b05427b0
Mindy927 <lionetmin@icloud.com>
2018-09-17 20:38:50 -0400

    merge sort

--- 493-Reverse Pairs.java
+++ 493-Reverse Pairs.java
@@ -24,6 +24,60 @@ merge sort, divide array into two parts cnt(left, right) = cnt(left, mid) + cnt(
 C is number of reverse pairs (i,j) such that i in [left, mid], j in [mid+1, right] , calculated using two pointers
 */
 
+//version 1
+class Solution { 
+    int[] copy; //global array to copy from orginal nums array when we perform merge sort
+    public int reversePairs(int[] nums) {
+        copy = new int[nums.length];
+        return merge(nums, 0, nums.length-1);
+    }
+    
+    //number of reverse pairs in range nums[left] to nums[right]
+    public int merge(int[] nums, int left, int right){
+        if (left >= right) return 0;
+        
+        int mid = left + (right - left)/2;
+        int cnt = 0;
+        
+        cnt += merge(nums, left, mid); //nums[left] to nums[mid] is already sorted after calling helper function
+        cnt += merge(nums, mid+1, right);
+        
+        //
+        int i = left, j = mid + 1;
+        while (i<=mid){ 
+            //check boundary of j in the inner while loop
+            while (j<=right && nums[i]/2.0 > nums[j]) j++;
+            cnt += j - (mid+1);
+            i++;
+        }
+        //cnt number of reverse pair(i,j) with i in range[start, mid], j in range[mid+1, end]
+        mergeSort(nums, left, mid, right);
+        return cnt;
+    }
+    
+    //nums[left] to nums[mid] are already sored when we call merge above
+    public void mergeSort(int[] nums, int left, int mid, int right){
+        if (left >= right) return; 
+        //make of copy of original array nums, read from copy array and modify nums array
+        for (int i=left; i<=right; i++) copy[i] = nums[i]; 
+      
+        int p1 = left;
+        int p2 = mid + 1;
+        int index = left;
+        while (index <= right){
+            //read from copy array for original data
+            if (p1 > mid || (p2<= right && copy[p2] <= copy[p1])) nums[index++] = copy[p2++];
+            else nums[index++] = copy[p1++];
+        }
+        
+    }
+}
+
+
+
+
+
+//version 1
 class Solution {
     public int reversePairs(int[] nums) {
         return merge(nums, 0, nums.length-1);
