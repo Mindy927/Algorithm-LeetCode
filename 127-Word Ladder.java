f2424e7bc30ec1557432ffaeac5c143b2f5d2dd7
Mindy927 <lionetmin@icloud.com>
2018-09-19 16:02:06 -0400

    bfs

--- 127-Word Ladder.java
+++ 127-Word Ladder.java
@@ -34,40 +34,104 @@ Explanation: The endWord "cog" is not in wordList, therefore no possible transfo
 
 Author: Mindy927 */
 
+//two-end bfs
+/*
+- beginSet start from beginWord, endSet start from endWord
+- visited store all strings have been visited so far
+- use temp set to store words to be searched in next round
+
+*/
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        //for the bi-directional graph, begin set keeps elements in begin set at biggest depth(elements added in last round)
+        Set<String> beginSet = new HashSet<>(); 
+        Set<String> endSet = new HashSet<>();
+        Set<String> dict = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+        
+        for (String word:wordList) dict.add(word);
+        if (!dict.contains(endWord))  return 0;
+            
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        
+        int dist = 1;
+        while (!beginSet.isEmpty() && !endSet.isEmpty()){
+            dist++;
+            //make sure begin set is smaller
+            if (beginSet.size() > endSet.size()){
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
+            
+            //for each string in beginSet, modify 1 char and check whether it can reach endSet
+            Set<String> temp = new HashSet<>(); //string to be searched in next round
+            for(String str:beginSet){
+                char[] chars = str.toCharArray();
+                for (int i=0; i<chars.length; i++){
+                    char prev = chars[i];
+                    for (char c='a'; c<='z'; c++){
+                        chars[i] = c;
+                        String next = String.valueOf(chars);
+                        if (endSet.contains(next)) return dist;
+                        if (dict.contains(next) && !visited.contains(next)) {
+                            temp.add(next);
+                            visited.add(next);
+                        }
+                    }
+                    chars[i] = prev; //convert ith char back
+                }
+            }
+            beginSet = temp;
+        }
+        
+        return 0;
+    }
+}
+
+
+
 //bfs with used set(or remove visited string from dict set)
 //bfs with used set
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> dict = new HashSet<>();
-        for (String word:wordList) dict.add(word);
-        
-        if (!dict.contains(endWord)) return 0; //corner case
+        Set<String> visited = new HashSet<>();
         Queue<String> q = new LinkedList<>();
-        q.offer(beginWord);
-        int level = 1;
+        int level = 0;
         
-        while (!q.isEmpty()){
+        for (String word:wordList){
+            dict.add(word);
+        }
+        if (!dict.contains(endWord)) return 0;
+        
+        q.offer(beginWord);
+        visited.add(beginWord);
+        while(!q.isEmpty()){
             level++;
             int size = q.size();
-            for(int i=0; i<size; i++){
-                char[] chars = q.poll().toCharArray();
-                for (int j=0; j<chars.length; j++){ //replace s.charAt(j) with c
-                    for (char c = 'a'; c<='z'; c++){
-                        char prev = chars[j];
-                        if (chars[j] == c) continue;
-                        chars[j] = c;
+            for(int s=0; s<size; s++){
+                String word = q.poll();
+                char[] chars = word.toCharArray();
+                for (int i=0; i<word.length(); i++){
+                    char prev = chars[i];
+                    for (char c = 'a'; c < 'z'; c++){
+                        chars[i] = c;
                         String temp = String.valueOf(chars);
-                        if (temp.equals(endWord)) return level;
-                        if (dict.contains(temp)) {
+                        if (temp.equals(endWord)) return level+1;
+                        if (!visited.contains(temp) && dict.contains(temp)){
+                            visited.add(temp);
                             q.offer(temp);
-                            dict.remove(temp);
                         }
-                        chars[j] = prev;  //Dont forget to revert!
-                    } 
+                        chars[i] = prev;
+                    }
                 }
             }
         }
         
         return 0;
+        
     }
 }
