4826814b6be3f74d6835e4efbcf7c6e874800cf0
Mindy927 <lionetmin@icloud.com>
2018-09-23 17:03:11 -0400

    sliding window

--- 713-Subarray Product Less Than K.java
+++ 713-Subarray Product Less Than K.java
@@ -16,7 +16,30 @@ Note:
 
 Authour: Mindy927*/
 
+//version 1: sliding window: find max
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int cnt = 0, prod = 1;
+        int left = 0, right = 0;
+        
+        while (right < nums.length){
+            prod *= nums[right];
+            
+            while ( left <= right && prod >= k){ //while(invalid) for findMax case
+                prod = prod/nums[left];
+                left++;
+            }
+            //for each right pointer, find left most element such that product from left to right < k
+            cnt += right - left + 1;
+            right++;
+        }
+        
+        return cnt;
+    }
+}
 
+
+//version 2
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         // keep max prod window less than k, iterates through j, find max i such that prod[i,j] < k
