9aea8c832d5863939364454455fe7986ef4b8253
Mindy927 <lionetmin@icloud.com>
2018-10-16 16:01:04 -0400

    dfs+dp

--- 329-Longest Increasing Path in a Matrix.java
+++ 329-Longest Increasing Path in a Matrix.java
@@ -1,5 +1,4 @@
 /*
-
 Given an integer matrix, find the length of the longest increasing path.
 
 From each cell, you can either move to four directions: left, right, up or down. You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
@@ -23,46 +22,43 @@ Input: nums =
   [2,2,1]
 ] 
 Output: 4 
-Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+Explanation: The longest increasing path is [3, 4, 5, 6]. 
 
 Author: Mindy927*/
 
- //Memorize:if the result for a cell is not calculated, we calculate and cache it; otherwise, we get it from the cache directly
-
+// dfs + memorize:if the result for a cell is not calculated, we calculate and cache it; otherwise, we get it from the cache directly
 class Solution {
-    static final int[] DIRS = new int[]{0,1,0,-1,0};
     public int longestIncreasingPath(int[][] matrix) {
-        if (matrix.length==0) return 0;
-        int[][] memorize = new int[matrix.length][matrix[0].length]; 
-       
-        for (int i=0; i<matrix.length; i++){
-            for (int j=0; j<matrix[0].length; j++){
-                 dfs(matrix, memorize, i, j);
-             }
-        }
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int res = 0;
+        int[][] dp = new int[m][n]; //longest increasing path start from dp[i][j]
         
-        int max = 0;
-        for (int i=0; i<matrix.length; i++){
-            for (int j=0; j<matrix[0].length; j++){
-                max = Math.max(memorize[i][j], max);
+        for (int i=0; i<m; i++) Arrays.fill(dp[i], -1);
+        
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                res = Math.max(res, dfs(matrix, i, j, dp));
             }
         }
         
-        return max+1; 
+        return res;
     }
     
-
-    public void dfs(int[][] matrix,int[][] memorize, int i, int j){
-       
-        if (memorize[i][j] !=0) return; // Has been calculated
+    public static int[] DIRS = new int[]{1, 0, -1, 0, 1};
+    
+    public int dfs(int[][] matrix, int i, int j, int[][] dp){
+        if (dp[i][j] != -1) return dp[i][j];
         
+        int temp = 1;
         for (int d=0; d<4; d++){
             int x = i + DIRS[d];
             int y = j + DIRS[d+1];
-            
-            if (x<0 || y<0 || x>=matrix.length || y>= matrix[0].length || matrix[x][y] >= matrix[i][j]) continue;
-            dfs(matrix, memorize, x, y); //update memorize based on neighbor[x,y]
-            memorize[i][j] = Math.max(memorize[i][j], 1 + memorize[x][y]);
+            if (x>=0 && x<matrix.length && y>=0 && y<matrix[0].length && matrix[x][y] > matrix[i][j]) temp = Math.max(temp, dfs(matrix, x, y, dp)+1);
         }
+        
+        dp[i][j] = temp;
+        return temp;
     }
 }
