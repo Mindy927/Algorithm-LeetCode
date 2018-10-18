5e6c508803407e5a1aa7e89a1b0f806eb4e4a5af
Mindy927 <lionetmin@icloud.com>
2018-10-18 15:41:27 -0400

    trie

--- 745-Prefix and Suffix Search.java
+++ 745-Prefix and Suffix Search.java
@@ -16,56 +16,61 @@ prefix, suffix have lengths in range [0, 10].
 words[i] and prefix, suffix queries consist of lowercase letters only.
 Author: Mindy927*/
 
+/*
+wrap each word with all possible prefix + "{" + reversedWord
+example: apple => {eppla, a{eppla, ap{eppla, app{eppla, appl{elppa ..
+When search prefix "a", suffix "e" => search "a{e"
 
+*/
 class WordFilter {
     class TrieNode{
-        TrieNode[] children = new TrieNode[27]; //including '{'
-        char val;
-        int weight = -1;
-        public TrieNode(){};
-        public TrieNode(char c){
-            this.val = c;
-        }
+        int index = -1;
+        TrieNode[] children = new TrieNode[27]; //27th char for "{"
+        public TrieNode(){}        
     }
     
     TrieNode head;
     public WordFilter(String[] words) {
-            head = new TrieNode();
-            for (int i=0; i<words.length; i++){
-                String word = words[i];
-                for (int j=0; j<=word.length(); j++){  //wrap each word with all possible suffix(word.substring(j))
-                    String temp = word.substring(j) + "{" + word;
-                    insert(temp, i);
-                }
+        head = new TrieNode(){};
+        for (int i=0; i<words.length; i++){
+            StringBuilder sb = new StringBuilder(words[i]);
+            String reverse = sb.reverse().toString();
+            for (int j=0; j<=words[i].length(); j++){ //wrap each word with all possible suffix(word.substring(j))
+                String temp = words[i].substring(0,j) + "{" +  reverse; 
+                addToDict(temp, i);
             }
+        }    
     }
     
-    public void insert(String word, int w){
+    public void addToDict(String word, int index){
         TrieNode cur = head;
         for (int i=0; i<word.length(); i++){
-            if (cur.weight < w) cur.weight = w;
+            cur.index = index;
             char c = word.charAt(i);
-            if (cur.children[c - 'a'] == null) cur.children[c - 'a'] = new TrieNode(c);
+            if (cur.children[c - 'a'] == null) cur.children[c - 'a'] = new TrieNode();
             cur = cur.children[c - 'a'];
         }
-        if (cur.weight < w) cur.weight = w;
+        cur.index = index; //overwrite previous index
     }
-    
+
     public int f(String prefix, String suffix) {
-        String temp = suffix + '{' + prefix;
-        return search(temp);
+             StringBuilder sb = new StringBuilder(suffix); //reverse suffix before searching
+            String reverse = sb.reverse().toString();
+            return search(prefix + "{" + reverse);
     }
     
     public int search(String word){
         TrieNode cur = head;
         for (int i=0; i<word.length(); i++){
             char c = word.charAt(i);
-            if (cur.children[c - 'a'] == null) return -1;
+            if (cur.children[ c - 'a'] == null) return -1;
             cur = cur.children[c - 'a'];
         }
-        return cur.weight;
+        return cur.index;
     }
+    
 }
+
 
 /**
  * Your WordFilter object will be instantiated and called as such:
