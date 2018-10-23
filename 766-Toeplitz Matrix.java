e2af8958864b9260899f27aecc987f3430bf919e
Mindy927 <lionetmin@icloud.com>
2018-10-22 20:19:25 -0400

    matrix

--- 766-Toeplitz Matrix.java
+++ 766-Toeplitz Matrix.java
@@ -17,39 +17,41 @@ Explanation:
 In the above grid, the diagonals are:
 "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
 In each diagonal all elements are the same, so the answer is True.
-Example 2:
 
-Input:
-matrix = [
-  [1,2],
-  [2,2]
-]
-Output: False
-Explanation:
-The diagonal "[1, 2]" has different elements.
-
-Note:
-
-matrix will be a 2D array of integers.
-matrix will have a number of rows and columns in range [1, 20].
-matrix[i][j] will be integers in range [0, 99].
 
 Follow up:
+1.What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+  -- Store one row and compare row by row
+2.What if the matrix is so large that you can only load up a partial row into the memory at once?
 
-What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
-What if the matrix is so large that you can only load up a partial row into the memory at once?
 Author: Mindy927*/
 
-//hash map <row-col, val>
+//O(1) space, compare each val with its right-down value
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        Map<Integer, Integer> map = new HashMap<>();
+        
         for (int i=0; i<matrix.length; i++){
             for (int j=0; j<matrix[0].length; j++){
-                if (!map.containsKey(i-j)) map.put(i-j, matrix[i][j]);
-                else {
-                    if (map.get(i-j) != matrix[i][j]) return false;
-                }
+                if (i+1 < matrix.length && j+1 < matrix[0].length && matrix[i][j] != matrix[i+1][j+1]) return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+// O(m+n) space, good for follow-up 1
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] diag = new int[m+n-1]; //diagnal[i]: value for ith diagnal(left-bottom to upper-right)
+        Arrays.fill(diag, -1);
+        
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (diag[ j - i + m-1] != -1 && matrix[i][j] != diag[ j - i + m-1]) return false;
+                diag[ j - i + m-1] = matrix[i][j];
             }
         }
         
